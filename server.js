a2be56e3dd87dae6f143adb03d831ef2731ed660
Ayman Naif <ayman_naif@hotmail.com>
2021-05-03 22:34:07 +0300

    finish lab 11

--- server.js
+++ server.js
@@ -11,25 +11,31 @@ server.use(cors());
 server.set('view engine','ejs');
 server.use(express.static('./public'));
 
+server.use(express.urlencoded({ extended: true }));
 
 server.get('/', (req, res) => {
   res.render('./pages/index')
 });
 
-server.get('/show', booksHandelr)
+server.get('/search', (req, res) => {
+  res.render('./pages/searches/show.ejs')
+});
+server.post('/search/new', booksHandelr)
 
 //  books Data ................
 function booksHandelr(req, res) {
-  let search = req.query.q;
-  let booksUrl = `https://www.googleapis.com/books/v1/volumes?q=${search}`;
+  let search = req.body.search;
+  let term = req.body.radio
+
+  let booksUrl = `https://www.googleapis.com/books/v1/volumes?q=+${search}:${term}`;
   superagent.get(booksUrl)
     .then((bookData) => {
       let boData = bookData.body;
 
       let booksArr = boData.items.map((item) =>new Books(item));
 
-      //res.status(200).send(booksArr);
-      res.render('./pages/searches/show',{booksData:booksArr})
+      // res.status(200).send(booksArr);
+      res.render('./pages/searches/new.ejs',{booksData:booksArr})
 
     })
     .catch(error => {
@@ -39,10 +45,10 @@ function booksHandelr(req, res) {
 }
 
 function Books(bookData) {
-  this.title = bookData.volumeInfo.title;
-  this.authors = bookData.volumeInfo.authors;
-  this.description = bookData.volumeInfo.description;
-  this.img_url = bookData.volumeInfo.imageLinks;
+  this.title = bookData.volumeInfo.title|| 'N/A';
+  this.authors = bookData.volumeInfo.authors|| 'N/A';
+  this.description = bookData.volumeInfo.description || 'N/A';
+  this.img_url = bookData.volumeInfo.imageLinks.smallThumbnail || bookData.volumeInfo.imageLinks.thumbnail || `https://i.imgur.com/J5LVHEL.jpg`;
 
 }
 
